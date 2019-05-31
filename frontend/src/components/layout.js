2dd1034dd6f678bd3ecade2eb4c2a972459cfcc5
Juicyjames <iceandjames@gmail.com>
2019-05-30 19:17:43 -0600

    Setup styled components and Reakit with theming support

--- frontend/src/components/layout.js
+++ frontend/src/components/layout.js
@@ -8,7 +8,9 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import { ThemeProvider } from "styled-components"
 
+import theme from "./theme"
 import Header from "./header"
 import "./layout.css"
 
@@ -24,7 +26,8 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
+      <ThemeProvider theme={theme}>
+        <>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
@@ -41,7 +44,8 @@ const Layout = ({ children }) => (
             <a href="https://www.gatsbyjs.org">Gatsby</a>
           </footer>
         </div>
-      </>
+        </>
+      </ThemeProvider>
     )}
   />
 )
