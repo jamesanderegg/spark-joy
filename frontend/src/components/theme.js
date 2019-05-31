15988ba7fff75617850fb7f208ad074478d8e56d
Juicyjames <iceandjames@gmail.com>
2019-05-31 17:21:48 -0600

    started using rebass add export button

--- frontend/src/components/theme.js
+++ frontend/src/components/theme.js
@@ -2,6 +2,9 @@ export default {
     //add color pallets
     palette:{
         headings: ['2.25rem', '1.62rem','1.38rem','1rem','0.85rem'],
+    },
+    colors: {
+        primary: 'magenta'
     }
 }
 
