d13ffab623c9b8c9ec1c9a4715cab8503f5ba24a
Juicyjames <iceandjames@gmail.com>
2019-05-30 20:20:54 -0600

    Basic page layout

---
+++ frontend/src/components/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+import { palette } from "styled-tools"
+
+export const CentralColumn = styled.div`
+    margin: 0 auto;
+    max-width: 960px;
+`
+
+export const Heading = styled.h1`
+    font-size: ${palette("headings",0)};
+    line-height: ${palette("headings", 0)};
+    margin-bottom: 0;
+`
--- frontend/src/components/theme.js
+++ frontend/src/components/theme.js
@@ -1,3 +1,7 @@
 export default {
     //add color pallets
+    palette:{
+        headings: ['2.25rem', '1.62rem','1.38rem','1rem','0.85rem'],
+    }
 }
+
